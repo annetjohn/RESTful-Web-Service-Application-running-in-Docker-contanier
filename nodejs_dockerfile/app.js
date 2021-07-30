ac4e48171870acc5650d2217ef6dd1da37dddacb
annetjohn <annetjohn.john@gmail.com>
2021-07-30 01:36:24 -0400

    Added comments on app.js file.

--- nodejs_dockerfile/app.js
+++ nodejs_dockerfile/app.js
@@ -8,14 +8,18 @@ let rawdata = fs.readFileSync('customer.json');
 let customer = JSON.parse(rawdata);
 console.log(customer);
 
+
 app.get('/', function (_req, res){
-    res.send("Cafe - Sweet Desserts")
+    res.send("Cafe - Sweet Desserts")   // request and response - client/server model.
 })
 
+//One that displays a collection of records
 app.get('/customers', function (_req, res){
-    res.end(JSON.stringify(customer))
+    res.end(JSON.stringify(customer))  //stringfy - converting JavaScript object into a string
 })
 
+
+//One that displays a collection of records for a given entity.
 app.get('/customers/:id/addons', function (req, res){
     var id = req.params.id
     console.log(customer["customer"+id]);
@@ -28,12 +32,15 @@ app.get('/customers/:id/addons', function (req, res){
 
 })
 
+//One that displays a single record that corresponds to an ID.
 app.get('/customers/:id', function (req, res){
     var customers = JSON.parse(rawdata)
     var customer = customers["customer"+req.params.id]
 
     res.end(JSON.stringify(customer))
 })
+
+//One that displays a single record from a collection of a given entity. 
 app.get('/customers/:id/addons/:addId', function (req, res){
     var id = req.params.id;
     var addId = req.params.addId;
